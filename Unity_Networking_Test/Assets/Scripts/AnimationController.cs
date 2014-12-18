d0f5d010143a4bf8f0b77f2b504612fe3529e17e
Li Zhi He <hlz1991@hotmail.com>
2014-12-18 13:18:47 -0500

    Basic Mechanim implementation, jump animation is very weird right now.

--- Unity_Networking_Test/Assets/Scripts/AnimationController.cs
+++ Unity_Networking_Test/Assets/Scripts/AnimationController.cs
@@ -5,6 +5,7 @@ public class AnimationController : MonoBehaviour {
 
 	//Fields
 	public Animation animation;
+	public Animator animator;
 	PlayerManager playerManager;
 
 
@@ -18,19 +19,29 @@ public class AnimationController : MonoBehaviour {
 	void Update () {
 		switch (playerManager.movementState) {
 		case PlayerManager.MovementState.IDLE:
+			animator.SetBool ("RunCheck", false);
+			animator.SetBool ("directionPress", false);
+			animator.SetBool ("jump", false);
 			animation.enabled = true;
 			animation.Play ("idle");
 			break;
 		case PlayerManager.MovementState.WALKING:
+			animator.SetBool ("RunCheck", false);
+			animator.SetBool ("directionPress", true);
+			animator.SetBool ("jump", false);
 			animation.enabled = true;
 			animation.Play ("walk");
 			break;
 		case PlayerManager.MovementState.RUNNING:
+			animator.SetBool ("RunCheck", true);
+			animator.SetBool ("directionPress", true);
+			animator.SetBool ("jump", false);
 			animation.enabled = true;
 			animation.Play ("run");
 			break;
 		case PlayerManager.MovementState.Jumping:
 			animation["jump_pose"].wrapMode = WrapMode.Once;
+			animator.SetBool ("jump", true);
 			//if (!animation.isPlaying) {
 			if (!animation.IsPlaying ("jump_pose")) {
 				animation.Play ("jump_pose");
