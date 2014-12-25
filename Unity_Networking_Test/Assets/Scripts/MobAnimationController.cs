0779f0ba95949df01cea65069e2a7fccd74c278b
Li Zhi He <hlz1991@hotmail.com>
2014-12-24 21:09:47 -0800

    Zombies. Networking. Done!!!!!!!

--- Unity_Networking_Test/Assets/Scripts/MobAnimationController.cs
+++ Unity_Networking_Test/Assets/Scripts/MobAnimationController.cs
@@ -5,53 +5,112 @@ public class MobAnimationController : MonoBehaviour {
 
 	//Fields
 	float speed;
-
+	bool investigate, chase, isAttacking, receiveDamage, isDead;
+	Animator myAnimator;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		myAnimator = GetComponent<Animator> ();
 	}
+
+	[RPC]
+	void UpdateAnimation (bool investigate, bool chase, bool isAttacking, bool receiveDamage, bool isDead) {
+		myAnimator.SetBool ("investigate", true);
+//		myAnimator.SetBool ("chase", chase);
+//		myAnimator.SetBool ("isAttacking", isAttacking);
+//		myAnimator.SetBool ("receiveDamage", receiveDamage);
+//		myAnimator.SetBool ("isDead", isDead);
+	}
+
+	[RPC]
+	void UpdateInvestigate (bool investigate) {
+		myAnimator.SetBool ("investigate", investigate);
+	}
+	[RPC]
+	void UpdateChase (bool chase) {
+		myAnimator.SetBool ("chase", chase);
+	}
+	[RPC]
+	void UpdateIsAttacking (bool isAttacking) {
+		myAnimator.SetBool ("isAttacking", isAttacking);
+	}
+	[RPC]
+	void UpdateReceiveDamage (bool receiveDamage) {
+		myAnimator.SetBool ("receiveDamage", receiveDamage);
+	}
+	[RPC]
+	void UpdateIsDead (bool isDead) {
+		myAnimator.SetBool ("isDead", isDead);
+	}
+
 	
 	// Update is called once per frame
 	void Update () {
-		if (GetComponent<HealthManager>().isTakingDamage) {
-			if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("ReceiveDamage")) {
-				GetComponent<Animator> ().SetBool ("receiveDamage", false);
-				GetComponent<HealthManager>().isTakingDamage = false;
+		if (networkView.isMine) {
+			if (GetComponent<HealthManager>().isTakingDamage) {
+				if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("ReceiveDamage")) {
+					GetComponent<Animator> ().SetBool ("receiveDamage", false);
+					GetComponent<HealthManager>().isTakingDamage = false;
+					receiveDamage = false;
+					networkView.RPC ("UpdateReceiveDamage", RPCMode.All, false);
+				}
+				else {
+					GetComponent<Animator>().speed = 1;
+					GetComponent<Animator> ().SetBool ("receiveDamage", true);
+					receiveDamage = true;
+					networkView.RPC ("UpdateReceiveDamage", RPCMode.All, true);
+				}
+			}
+
+			if (GetComponent<HealthManager>().isDead) {
+				GetComponent<Animator> ().SetBool ("isDead", true);
+				isDead = true;
+				networkView.RPC ("UpdateIsDead", RPCMode.All, true);
+			}
+
+			if (GetComponent<MobMovement>().isAttacking &&
+			    !GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Attack")) {
+				GetComponent<Animator> ().SetBool ("isAttacking", true);
+				GetComponent<MobMovement>().isAttacking = false;
+				isAttacking = true;
+				networkView.RPC ("UpdateIsAttacking", RPCMode.All, true);
+			}
+			else if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Attack")) {
+				GetComponent<Animator> ().SetBool ("isAttacking", false);
+				isAttacking = false;
+				networkView.RPC ("UpdateIsAttacking", RPCMode.All, false);
+			}
+
+			Animator animator = GetComponent<Animator> ();
+			MobMovement mobMovement = GetComponent<MobMovement> ();
+			speed = (rigidbody.velocity - new Vector3 (0, rigidbody.velocity.y, 0)).magnitude;
+			if (mobMovement.targetAquired) {
+				animator.SetBool ("investigate", false);
+				animator.SetBool ("chase", true);
+				investigate = false;
+				chase = true;
+				networkView.RPC ("UpdateInvestigate", RPCMode.All, false);
+				networkView.RPC ("UpdateChase", RPCMode.All, true);
+			}
+			else if (mobMovement.isSuspicious) {
+				animator.SetBool ("investigate", true);
+				animator.SetBool ("chase", false);
+				investigate = true;
+				chase = false;
+				networkView.RPC ("UpdateInvestigate", RPCMode.All, true);
+				networkView.RPC ("UpdateChase", RPCMode.All, false);
 			}
 			else {
-				GetComponent<Animator>().speed = 1;
-				GetComponent<Animator> ().SetBool ("receiveDamage", true);
+				animator.SetBool ("investigate", false);
+				animator.SetBool ("chase", false);
+				investigate = false;
+				chase = false;
+				networkView.RPC ("UpdateInvestigate", RPCMode.All, false);
+				networkView.RPC ("UpdateChase", RPCMode.All, false);
 			}
 		}
-
-		if (GetComponent<HealthManager>().isDead) {
-			GetComponent<Animator> ().SetBool ("isDead", true);
-		}
-
-		if (GetComponent<MobMovement>().isAttacking &&
-		    !GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Attack")) {
-			GetComponent<Animator> ().SetBool ("isAttacking", true);
-			GetComponent<MobMovement>().isAttacking = false;
-		}
-		else if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("Attack")) {
-			GetComponent<Animator> ().SetBool ("isAttacking", false);
-		}
-
-		Animator animator = GetComponent<Animator> ();
-		MobMovement mobMovement = GetComponent<MobMovement> ();
-		speed = (rigidbody.velocity - new Vector3 (0, rigidbody.velocity.y, 0)).magnitude;
-		if (mobMovement.targetAquired) {
-			animator.SetBool ("investigate", false);
-			animator.SetBool ("chase", true);
-		}
-		else if (mobMovement.isSuspicious) {
-			animator.SetBool ("investigate", true);
-			animator.SetBool ("chase", false);
-		}
 		else {
-			animator.SetBool ("investigate", false);
-			animator.SetBool ("chase", false);
+			//networkView.RPC ("UpdateAnimation", RPCMode.All, investigate, chase, isAttacking, receiveDamage, isDead);
 		}
 	}
 }
--- Unity_Networking_Test/Assets/Scripts/MobMovement.cs
+++ Unity_Networking_Test/Assets/Scripts/MobMovement.cs
@@ -15,56 +15,73 @@ public class MobMovement : MonoBehaviour {
 	
 	}
 
+	[RPC]
+	void UpdateState (bool isAtt, bool targAqui, bool isSus) {
+		isAttacking = isAtt;
+		targetAquired = targetAquired;
+		isSuspicious = isSus;
+	}
+
 	//Rotation
 	void FixedUpdate () {
-		if (GetComponent<HealthManager>().isDead || isAttacking) {
-			return;
-		}
-		if (targetAquired) {
-			Vector3 direction = (targetPosition - transform.position).normalized;
-			direction = direction - new Vector3 (0, direction.y, 0);
-			transform.forward = Vector3.Slerp (transform.forward, direction, 0.1f);
-			rigidbody.AddForce (direction * acceleration, ForceMode.Acceleration);
-			Vector3 temp = new Vector3 (rigidbody.velocity.x, 0, rigidbody.velocity.z);
-			if (temp.magnitude > chaseSpeed) {
-				rigidbody.velocity = temp.normalized * chaseSpeed;
+		if (networkView.isMine) {
+			if (GetComponent<HealthManager>().isDead || isAttacking) {
+				return;
+			}
+			if (targetAquired) {
+				Vector3 direction = (targetPosition - transform.position).normalized;
+				direction = direction - new Vector3 (0, direction.y, 0);
+				transform.forward = Vector3.Slerp (transform.forward, direction, 0.1f);
+				rigidbody.AddForce (direction * acceleration, ForceMode.Acceleration);
+				Vector3 temp = new Vector3 (rigidbody.velocity.x, 0, rigidbody.velocity.z);
+				if (temp.magnitude > chaseSpeed) {
+					rigidbody.velocity = temp.normalized * chaseSpeed;
+				}
+			}
+			else if (isSuspicious) {
+				Vector3 direction = (targetPosition - transform.position).normalized;
+				direction = direction - new Vector3 (0, direction.y, 0);
+				transform.forward = Vector3.Slerp (transform.forward, direction, 0.1f);
+				rigidbody.AddForce (direction * acceleration, ForceMode.Acceleration);
+				Vector3 temp = new Vector3 (rigidbody.velocity.x, 0, rigidbody.velocity.z);
+				if (temp.magnitude > investigateSpeed) {
+					rigidbody.velocity = temp.normalized * investigateSpeed;
+				}
 			}
 		}
-		else if (isSuspicious) {
-			Vector3 direction = (targetPosition - transform.position).normalized;
-			direction = direction - new Vector3 (0, direction.y, 0);
-			transform.forward = Vector3.Slerp (transform.forward, direction, 0.1f);
-			rigidbody.AddForce (direction * acceleration, ForceMode.Acceleration);
-			Vector3 temp = new Vector3 (rigidbody.velocity.x, 0, rigidbody.velocity.z);
-			if (temp.magnitude > investigateSpeed) {
-				rigidbody.velocity = temp.normalized * investigateSpeed;
-			}
+		else {
+
 		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-		targetAquired = false;
-		isSuspicious = false;
-		Collider[] colliders = Physics.OverlapSphere (transform.position, sightRange);
-		Collider[] sColliders = Physics.OverlapSphere (transform.position, suspicionRange);
-		foreach (Collider col in colliders) {
-			if (col.gameObject.GetComponent<PlayerManager>() != null) {
-				targetAquired = true;
-				targetPosition = col.gameObject.transform.position;
-			}
-		}
-		if (!targetAquired) {
-			foreach (Collider col in sColliders) {
+		if (networkView.isMine ) {
+			targetAquired = false;
+			isSuspicious = false;
+			Collider[] colliders = Physics.OverlapSphere (transform.position, sightRange);
+			Collider[] sColliders = Physics.OverlapSphere (transform.position, suspicionRange);
+			foreach (Collider col in colliders) {
 				if (col.gameObject.GetComponent<PlayerManager>() != null) {
-					isSuspicious = true;
+					targetAquired = true;
 					targetPosition = col.gameObject.transform.position;
 				}
 			}
+			if (!targetAquired) {
+				foreach (Collider col in sColliders) {
+					if (col.gameObject.GetComponent<PlayerManager>() != null) {
+						isSuspicious = true;
+						targetPosition = col.gameObject.transform.position;
+					}
+				}
+			}
+			if (targetAquired && Vector3.Distance (transform.position, targetPosition) < attackRange) {
+				isAttacking = true;
+			}
+			networkView.RPC ("UpdateState", RPCMode.All, isAttacking, targetAquired, isSuspicious);
 		}
-		if (targetAquired && Vector3.Distance (transform.position, targetPosition) < attackRange) {
-			print ("Attack!!!");
-			isAttacking = true;
+		else {
+			//networkView.RPC ("UpdateState", RPCMode.All, isAttacking, targetAquired, isSuspicious);
 		}
 	}
 }
